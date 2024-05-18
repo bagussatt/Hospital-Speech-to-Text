313bc1b739b033263ec98cd2b0c65cab93a3914b
Bagus Satrio <bagus.satrio.ft21@mail.umy.ac.id>
2024-05-18 17:52:46 +0700

    Updated Android build configuration, added internet and audio recording permissions, modified main.dart to use HomePage, registered SpeechToTextMacosPlugin, and updated pubspec.lock and pubspec.yaml with new dependencies.

---
+++ lib/screens/homepage.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:record_speechtotextfor_hospital/screens/recordpage.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _formKey = GlobalKey<FormState>(); // For form validation
+  final _nameController = TextEditingController(); // To manage text input
+
+  void _showConfirmationDialog(String name) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Konfirmasi'),
+          content: Text('Apakah Anda ingin melanjutkan ke halaman rekam medis untuk $name?'),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context, false), // Cancel
+              child: const Text('Batal'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context, true), // Confirm
+              child: const Text('Ya'),
+            ),
+          ],
+        );
+      },
+    ).then((confirmed) {
+      if (confirmed ?? false) {
+        // User confirmed, navigate to RecordPage
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => RecordPage(name: name)),
+        );
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Halaman Input Pasien'),
+      ),
+      body: Center(
+        child: Form(
+          key: _formKey, // Link form to validation key
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center, // Center column
+            children: [
+              Text(
+                "Pasien",
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.w500),
+              ),
+              SizedBox(
+                height: 35,
+              ),
+              TextFormField(
+                controller: _nameController, // Assign controller
+                decoration: InputDecoration(
+                  labelText: 'Nama Pasien:',
+                  border: OutlineInputBorder(), // Add border
+                  hintText: 'Masukan Nama Pasien', // Display hint text
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Nama Pasien Tidak Boleh Kosong.';
+                  }
+                  return null; // No error
+                },
+              ),
+              const SizedBox(height: 20), // Add spacing
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    // Form is valid
+                    String name = _nameController.text;
+                    _showConfirmationDialog(name); // Show confirmation dialog
+                  }
+                },
+                child: const Text('Submit'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/recordpage.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_to_text.dart'; // Import the speech_to_text package
+
+class RecordPage extends StatefulWidget {
+  final String name;
+
+  const RecordPage({super.key, required this.name});
+
+  @override
+  State<RecordPage> createState() => _RecordPageState();
+}
+
+class _RecordPageState extends State<RecordPage> {
+  final SpeechToText speechToText =
+      SpeechToText(); // Create SpeechToText instance
+  String kata = "Mengucapkan..."; // Initial text
+
+  @override
+  void initState() {
+    super.initState();
+    initializeSpeech();
+  }
+
+  void initializeSpeech() async {
+    bool available = await speechToText.initialize();
+    if (available) {
+      startListening();
+    }
+  }
+
+  void startListening() async {
+    speechToText.listen(
+      onResult: (result) {
+        setState(() {
+          kata = result.recognizedWords;
+        });
+      },
+      listenFor:
+          const Duration(minutes: 10), // Durasi maksimal untuk mendengarkan
+      pauseFor: const Duration(
+          seconds: 30), // Jeda sebelum menganggap tidak ada suara lagi
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    speechToText.stop(); // Stop listening when the page is disposed
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Rekaman'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Selamat datang, ${widget.name}!',
+              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            Text(kata), // Display the recognized speech
+          ],
+        ),
+      ),
+    );
+  }
+}
